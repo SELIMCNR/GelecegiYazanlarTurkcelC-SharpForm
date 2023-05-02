e0a790da8f9f32a807bcbf1d39a6a18edaea6ed3
Selim Çınar <90525336+SELIMCNR@users.noreply.github.com>
2023-05-02 16:45:11 +0300

    Add files via upload

---
+++ 3-Form-temelleri/Temel_Araç_Kullanımları/Form1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Temel_Araç_Kullanımları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label6.Text = "Selim Çınar"; //label 6 ya değeri yazdırıyor.
+            label6.Text = comboBox1.Text; // label 6'ya comboboxtan gelen değer eklendi.
+            textBox1.Text = "Reac Native"; // textbox1'e değer eklendi
+            label8.Text = textBox1.Text; // buttona tıklayınca textbox1 den gelen değeri yazdırır.
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Javascript"); // liste kutusuna ekler 
+            listBox1.Items.Add("PostegroSql");
+            comboBox1.Items.Add("Elazığ"); // Combobox kutusuna ekler.
+            listBox1.Items.Add(textBox1.Text);  // textbox1 'den gelen değeri ekle
+        }
+    }
+}
